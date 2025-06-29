607df883226798e8b7de5a17b60a94a68a960c53
owlsleep315 <chahwang0127@naver.com>
2025-06-29 15:10:32 +0900

    프로젝트 초기 설정

---
+++ src/main/java/me/owlsleep/owlab/OwlsleepApplication.java
@@ -0,0 +1,13 @@
+package me.owlsleep.owlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlsleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlsleepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/owlsleep/owlab/OwlsleepApplicationTests.java
@@ -0,0 +1,13 @@
+package me.owlsleep.owlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlsleepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
