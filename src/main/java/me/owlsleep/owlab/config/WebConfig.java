c23f71aa46dcd11038cfd287ae52efa7fdad102f
owlsleep315 <chahwang0127@naver.com>
2025-09-03 15:50:39 +0900

    https 주소 CORS 추가

--- src/main/java/me/owlsleep/owlab/config/WebConfig.java
+++ src/main/java/me/owlsleep/owlab/config/WebConfig.java
@@ -1,7 +1,11 @@
 package me.owlsleep.owlab.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +22,17 @@ public class WebConfig implements WebMvcConfigurer {
                         "/board/comment/delete/**",
                         "/board/*/reaction"
                 );
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://api.owlsleep.me",
+                        "https://couponmoa.netlify.app",
+                        "http://localhost:3000",
+                        "http://43.200.180.72:8080"
+                )
+                .allowedMethods("GET","POST","PATCH","PUT","DELETE","OPTIONS");
     }
 }
